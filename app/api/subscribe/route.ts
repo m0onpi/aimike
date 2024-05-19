171bb4e7cc6c48bec184c021dc3b376be6455064
m0onpi <michael.pigottbiz@gmail.com>
2024-05-19 17:37:20 +0100

    push

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -1,19 +1,29 @@
-// app/api/subscribe/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import sendgrid from '@sendgrid/mail';
 
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+const sendgridApiKey = process.env.SENDGRID_API_KEY;
+
+if (!sendgridApiKey) {
+  throw new Error('SENDGRID_API_KEY is not defined in environment variables');
+}
+
+sendgrid.setApiKey(sendgridApiKey);
 
 export async function POST(req: NextRequest) {
-  const { email } = await req.json();
-
   try {
+    const { email } = await req.json();
+
+    if (!email) {
+      return NextResponse.json({ error: 'Email is required' }, { status: 400 });
+    }
+
     await sendgrid.send({
       to: email,
       from: 'your-email@example.com',
       subject: 'Subscription Confirmation',
       text: 'Thank you for subscribing to our mailing list!',
     });
+
     return NextResponse.json({ message: 'Subscription successful' });
   } catch (error) {
     return NextResponse.json({ error: 'Subscription failed' }, { status: 500 });
