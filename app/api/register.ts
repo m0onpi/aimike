f156cbb111e599832cb1278701a242ff01057344
Michael Piggott <michael.piggott@my-furniture.co.uk>
2024-05-22 15:37:00 +0100

    new

--- app/api/register.ts
+++ app/api/register.ts
@@ -2,15 +2,22 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import mailchimp from '@mailchimp/mailchimp_marketing';
 
 mailchimp.setConfig({
-  apiKey: process.env.MAILCHIMP_API_KEY,
-  server: process.env.MAILCHIMP_SERVER_PREFIX,
+  apiKey: process.env.MAILCHIMP_API_KEY || '',
+  server: process.env.MAILCHIMP_SERVER_PREFIX || '',
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { name, email } = req.body;
+    const listId = process.env.MAILCHIMP_LIST_ID || '';
+    
+    // Check if any of the required variables are empty
+    if (!mailchimp.apiKey || !mailchimp.server || !listId) {
+      return res.status(500).json({ message: 'Server configuration error' });
+    }
+
     try {
-      const response = await mailchimp.lists.addListMember(process.env.MAILCHIMP_LIST_ID, {
+      const response = await mailchimp.lists.addListMember(listId, {
         email_address: email,
         status: 'subscribed',
         merge_fields: {
