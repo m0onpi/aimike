bd1a24f6024b2675a67a37bb9b5a671d0fa13c8b
Michael Piggott <michael.piggott@my-furniture.co.uk>
2024-05-23 12:30:27 +0100

    new

--- components/News.js
+++ components/News.js
@@ -43,25 +43,25 @@ export default function Subscribe() {
     <div className="max-w-md mx-auto p-4 bg-grey rounded shadow-md">
       <h1 className="text-center font-bold text-2xl mb-4">Register Your Interest</h1>
       <form onSubmit={subscribe} className="space-y-4">
-        <input className="w-full px-4 py-2 border rounded" 
+        <input className="w-full px-4 py-2 border rounded text-gray-800" 
           type="text" 
           placeholder="First Name" 
           ref={firstNameRef} 
           required 
         />
-        <input className="w-full px-4 py-2 border rounded" 
+        <input className="w-full px-4 py-2 border rounded text-gray-800" 
           type="text" 
           placeholder="Last Name" 
           ref={lastNameRef} 
           required 
         />
-        <input className="w-full px-4 py-2 border rounded" 
+        <input className="w-full px-4 py-2 border rounded text-gray-800" 
           type="email" 
           placeholder="Email" 
           ref={emailRef} 
           required 
         />
-        <input className="w-full px-4 py-2 border rounded" 
+        <input className="w-full px-4 py-2 border rounded text-gray-800" 
           type="text" 
           placeholder="Phone Number" 
           ref={phoneRef} 
@@ -69,7 +69,7 @@ export default function Subscribe() {
         />
         <div className="flex items-center">
         <span className="text-center mb-2">
-By submitting your email address, you agree to receiving marketing emails and promotions. You can unsubscribe at any time.</span>
+            By submitting your email address, you agree to receiving marketing emails and promotions. You can unsubscribe at any time.</span>
         </div>
         <button className="w-full bg-gray-700 text-white py-2 rounded hover:bg-gray-600" type="submit">
           Subscribe
