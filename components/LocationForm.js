29520c0098d0f9b8e6792cc3ef29136a91ccd79e
m0onpi <michael.pigottbiz@gmail.com>
2024-07-14 16:23:48 +0100

    leads-3

--- components/LocationForm.js
+++ components/LocationForm.js
@@ -38,12 +38,24 @@ const LocationForm = () => {
         }
     };
 
+    const formatContacts = (contacts) => {
+        return Object.entries(contacts).map(([key, value]) => {
+            if (Array.isArray(value)) {
+                return value.map((item, index) => (
+                    <div key={`${key}-${index}`}>{`${key}: ${item}`}</div>
+                ));
+            }
+            return <div key={key}>{`${key}: ${value}`}</div>;
+        });
+    };
+
     const exportToCSV = () => {
-        const headers = ["Name", "Address", "Rating", "Link"];
+        const headers = ["Name", "Address", "Rating", "Emails and Contacts", "Link"];
         const rows = localLeads.map(lead => [
             lead.name,
             lead.full_address,
             lead.rating,
+            JSON.stringify(lead.emails_and_contacts),
             lead.place_link
         ]);
 
@@ -60,76 +72,70 @@ const LocationForm = () => {
     };
 
     return (
-        <div className="max-w-md mx-auto p-4 bg-grey rounded shadow-md">
-            <h1 className="text-center font-bold text-2xl mb-4">Get GPS Coordinates and Local Leads</h1>
-            <form onSubmit={handleSubmit} className="space-y-4">
-                <input
-                    className="w-full px-4 py-2 border rounded text-gray-800"
-                    type="text"
-                    value={location}
-                    onChange={(e) => setLocation(e.target.value)}
-                    placeholder="Enter Location"
-                    required
-                />
-                <input
-                    className="w-full px-4 py-2 border rounded text-gray-800"
-                    type="text"
-                    value={category}
-                    onChange={(e) => setCategory(e.target.value)}
-                    placeholder="Enter Category"
-                    required
-                />
-                <button
-                    className="w-full bg-gray-700 text-white py-2 rounded hover:bg-gray-600"
-                    type="submit"
-                >
-                    Get Coordinates
-                </button>
-            </form>
-            {coordinates && (
-                <div className="mt-4 p-4 bg-white rounded shadow-md">
-                    <h2 className="text-xl font-bold ">Coordinates</h2>
-                    <p className="text-xl font-bold text-gray-800">{coordinates.lat}, {coordinates.lon}</p>
-                </div>
-            )}
-            {localLeads.length > 0 && (
-                <div className="mt-4 p-4 bg-white rounded shadow-md">
-                    <h2 className="text-xl font-bold text-gray-800">Local Leads</h2>
-                    <table className="min-w-full bg-white">
-                        <thead>
-                            <tr>
-                                <th className="py-2 px-4 border-b text-gray-800">Name</th>
-                                <th className="py-2 px-4 border-b text-gray-800">Address</th>
-                                <th className="py-2 px-4 border-b text-gray-800">Rating</th>
-                                <th className="py-2 px-4 border-b text-gray-800">Link</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {localLeads.map((lead, index) => (
-                                <tr key={index} className="hover:bg-gray-100">
-                                    <td className="py-2 px-4 border-b text-gray-800">{lead.name}</td>
-                                    <td className="py-2 px-4 border-b text-gray-800">{lead.full_address}</td>
-                                    <td className="py-2 px-4 border-b text-gray-800">{lead.rating}</td>
-                                    <td className="py-2 px-4 border-b text-gray-800">
-                                        <a href={lead.place_link} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">View on Map</a>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+        <div className="flex space-x-4 p-4">
+            <div className="w-1/3 bg-grey rounded shadow-md p-4">
+                <h1 className="text-center font-bold text-2xl mb-4">Get Leads From Area</h1>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <input
+                        className="w-full px-4 py-2 border rounded text-gray-800"
+                        type="text"
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
+                        placeholder="Enter Location"
+                        required
+                    />
+                    <input
+                        className="w-full px-4 py-2 border rounded text-gray-800"
+                        type="text"
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                        placeholder="Enter Category"
+                        required
+                    />
                     <button
-                        onClick={exportToCSV}
-                        className="w-full bg-blue-700 text-white py-2 mt-4 rounded hover:bg-blue-600"
+                        className="w-full bg-gray-700 text-white py-2 rounded hover:bg-gray-600"
+                        type="submit"
                     >
-                        Export to CSV
+                        Get Leads
                     </button>
-                </div>
-            )}
-            {error && (
-                <div className="mt-4 p-4 bg-red-200 text-red-800 rounded shadow-md">
-                    <p>{error}</p>
-                </div>
-            )}
+                </form>
+                {error && (
+                    <div className="mt-4 p-4 bg-red-200 text-red-800 rounded shadow-md">
+                        <p>{error}</p>
+                    </div>
+                )}
+            </div>
+            <div className="w-2/3 bg-white rounded shadow-md p-4">
+                {localLeads.length > 0 && (
+                    <div>
+                        <h2 className="text-xl font-bold text-gray-800">Local Leads</h2>
+                        <table className="min-w-full bg-white">
+                            <thead>
+                                <tr>
+                                    <th className="py-2 px-4 border-b text-gray-800">Name</th>
+                                    <th className="py-2 px-4 border-b text-gray-800">Address</th>
+                                    <th className="py-2 px-4 border-b text-gray-800">Emails and Contacts</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {localLeads.map((lead, index) => (
+                                    <tr key={index} className="hover:bg-gray-100">
+                                        <td className="py-2 px-4 border-b text-gray-800">{lead.name}</td>
+                                        <td className="py-2 px-4 border-b text-gray-800">{lead.full_address}</td>
+                                        <td className="py-2 px-4 border-b text-gray-800">{formatContacts(lead.emails_and_contacts)}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                        <button
+                            onClick={exportToCSV}
+                            className="w-full bg-blue-700 text-white py-2 mt-4 rounded hover:bg-blue-600"
+                        >
+                            Export to CSV
+                        </button>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
