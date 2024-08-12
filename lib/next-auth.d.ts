7858397674a455207191b081530b51dc6625c7ff
Michael Piggott <michael.piggott@my-furniture.co.uk>
2024-08-12 13:03:05 +0100

    fix

--- lib/next-auth.d.ts
+++ lib/next-auth.d.ts
@@ -1,12 +1,14 @@
 // types/next-auth.d.ts
 import NextAuth from "next-auth";
 import { JWT } from "next-auth/jwt";
+import { DefaultUser } from "next-auth";
+
 
 declare module "next-auth" {
-  interface User {
+  interface User extends DefaultUser {
     id: string;
     hasPaid: boolean;
-    password: string;
+    password: string | null;
   }
 
   interface Session {
