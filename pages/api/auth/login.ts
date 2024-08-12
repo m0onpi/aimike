1a5b25687eab4effc493e3aa7adf033bae89817a
Michael Piggott <michael.piggott@my-furniture.co.uk>
2024-08-12 12:33:12 +0100

    fix

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -4,7 +4,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from './../../../lib/prisma'; // Adjust this import based on your setup
 import { useRouter } from 'next/navigation';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
