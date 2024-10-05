2d51f3e5ab12f77cf816067a4bd29cbf2f57aa23
m0onpi <michael.pigottbiz@gmail.com>
2024-10-05 20:48:33 +0100

    added oil

--- pages/api/trade/sendBot.js
+++ pages/api/trade/sendBot.js
@@ -5,7 +5,8 @@ export default async function handler(req, res) {
     // Handle POST request
     try {
       const { message } = req.body;
-      console.log(message.json())
+      const data = message.json()
+      console.log(data)
       return res.status(200).json(message); // Exit after sending response
       
     } catch (error) {
